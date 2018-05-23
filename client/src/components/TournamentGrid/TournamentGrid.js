faa5a83990a2252db60fe325aef7d1c18f014894
Alex Space <spwanderer@mail.ru>
2018-05-23 20:26:56 +0600

    Added full grids makeup

--- client/src/components/TournamentGrid/TournamentGrid.js
+++ client/src/components/TournamentGrid/TournamentGrid.js
@@ -7,56 +7,116 @@ const TournamentGrid = (props) => {
     return (
         <div className='tournament-grid'>
 
-            <div className='grid-container'>
+            <div className='column-container'>
                 <header className='match-order-header'>
                     <h2>Round 1</h2>
                 </header>
-                <div className="pairs">
-                    <div className='pair-container'>
-                        <div className='top-player-container'>
-                            <span>Игрок первый</span>
-                            <div className='match-result'>
-                                <FontAwesome name='trophy-alt' />
+                <div className="grid-container">
+                    <div className="pairs">
+                        <div className='pair-container'>
+                            <div className='top-player-container'>
+                                <span>Игрок первый</span>
+                                <div className='match-result'>
+                                    <FontAwesome name='trophy-alt' />
+                                </div>
                             </div>
+                            <div className='bottom-player-container'>
+                                Игрок второй
                         </div>
-                        <div className='bottom-player-container'>
-                            Игрок второй
                         </div>
-                    </div>
 
-                    <div className='pair-container'>
-                        <div className='top-player-container'>
-                            <span>Игрок третий</span>
-                        </div>
-                        <div className='bottom-player-container'>
-                            Игрок четвертый
+                        <div className='pair-container'>
+                            <div className='top-player-container'>
+                                <span>Игрок третий</span>
+                            </div>
+                            <div className='bottom-player-container'>
+                                Игрок четвертый
                         <div className='match-result'>
-                                <FontAwesome name='trophy-alt' />
+                                    <FontAwesome name='trophy-alt' />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='pair-container'>
+                            <div className='top-player-container'>
+                                <span>Игрок первый</span>
+                                <div className='match-result'>
+                                    <FontAwesome name='trophy-alt' />
+                                </div>
+                            </div>
+                            <div className='bottom-player-container'>
+                                Игрок второй
+                        </div>
+                        </div>
+
+                        <div className='pair-container'>
+                            <div className='top-player-container'>
+                                <span>Игрок третий</span>
+                            </div>
+                            <div className='bottom-player-container'>
+                                Игрок четвертый
+                        <div className='match-result'>
+                                    <FontAwesome name='trophy-alt' />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='column-container'>
+                <header className='match-order-header'>
+                    <h2>Semifinal</h2>
+                </header>
+                <div className="grid-container">
+                    <div className="pairs">
+                        <div className='pair-container'>
+                            <div className='top-player-container'>
+                                <span>Игрок первый</span>
+                                <div className='match-result'>
+                                    <FontAwesome name='trophy-alt' />
+                                </div>
+                            </div>
+                            <div className='bottom-player-container'>
+                                Игрок четвертый
+                            </div>
+                        </div>
+                        <div className='pair-container'>
+                            <div className='top-player-container'>
+                                <span>Игрок первый</span>
+                                <div className='match-result'>
+                                    <FontAwesome name='trophy-alt' />
+                                </div>
+                            </div>
+                            <div className='bottom-player-container'>
+                                Игрок четвертый
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className='grid-container'>
+            <div className='column-container'>
                 <header className='match-order-header'>
-                    <h2>Round 2</h2>
+                    <h2>Final</h2>
                 </header>
-                <div className="pairs">
-                    <div className='pair-container'>
-                        <div className='top-player-container'>
-                            <span>Игрок первый</span>
-                            <div className='match-result'>
-                                <FontAwesome name='trophy-alt' />
+                <div className="grid-container">
+                    <div className="pairs">
+                        <div className='pair-container'>
+                            <div className='top-player-container'>
+                                <span>Игрок первый</span>
+                                <div className='match-result'>
+                                    <FontAwesome name='trophy-alt' />
+                                </div>
                             </div>
-                        </div>
-                        <div className='bottom-player-container'>
-                            Игрок четвертый
+                            <div className='bottom-player-container'>
+                                Игрок четвертый
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        
 
         </div>
     );
